814c201a59da89b7a772fb7aaa6c691bef03e0bf
Your Name <you@example.com>
2024-03-03 19:21:02 +0100

    njiji

--- code.js
+++ code.js
@@ -1,4 +1,7 @@
 //message console
 console.log("Hello World4")
 //alerte début
-alert("Bienvenue sur notre site web")
+let password=prompt('Veuillez entrez le mot de passe ')
+if (password=='WIKIFAMILLE'){
+    window.location.href='home.html';
+}
